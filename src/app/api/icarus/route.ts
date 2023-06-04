aa3bec646baa8766f65dcbd53cf97bbde51c44f4
Gabriel Soares <gabriel-soares@uergs.edu.br>
2023-06-04 00:23:53 -0300

    feature(api): call webhook

--- src/app/api/icarus/route.ts
+++ src/app/api/icarus/route.ts
@@ -1,6 +1,14 @@
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-    const message = `Server is flying!`;
+
+    // call https://daedalus.herokuapp.com/webhook/icarus
+    const message = await fetch('https://daedalus.herokuapp.com/webhook/icarus', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(res => res.json());
+    
     return NextResponse.json({ message }, { status: 200 });
 }
